8b79411652be59883a8f0825036d836a0c101f88
Fernando Gamba <fernandogamba19@gmail.com>
2024-05-06 16:56:49 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './core/guards/auth.guard';
+import { PrismaModule } from './core/prisma/prisma.module';
+import { AuthModule } from './features/auth/auth.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      privateKey: 'secret',
+    }),
+    ConfigModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+})
+export class AppModule {}
